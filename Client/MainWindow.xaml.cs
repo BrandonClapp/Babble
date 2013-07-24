d1ed01851a7f2783f95ac9d799f71918041aac45
Brandon <clapp09@gmail.com>
2013-07-23 21:21:18 -0500

    Initial commit

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClientInterface
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NewConnection_Click(object sender, RoutedEventArgs e)
+        {
+            AddActivity("Connecting");
+        }
+
+        private void AddActivity(string s)
+        {
+            ActivityTextBox.Text += "\n" + s;
+        }
+
+        private void Disconnect_Click(object sender, RoutedEventArgs e)
+        {
+            AddActivity("Disconnecting");
+        }
+    }
+}
